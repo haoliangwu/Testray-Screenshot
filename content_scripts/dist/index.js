f5d517ac7291e0d5c39288a082405120b6a88190
haoliang.wu <haoliang.wu@liferay.com>
2016-04-07 16:11:17 +0800

    add Testray Screenshot button

--- content_scripts/dist/index.js
+++ content_scripts/dist/index.js
@@ -1,3 +1,34 @@
-"use strict";
+'use strict';
 
-$(document).ready(function () {});
+$(document).ready(function () {
+  var comparePanel = $('.button-holder:last');
+
+  var screenshotButton = $('<button class="btn control-button btn-primary">Testray Screenshot</button>');
+
+  screenshotButton.click(function () {
+    var resultTable = $('.table-data');
+    var trList = resultTable.find('tr');
+
+    console.log(trList);
+
+    var result = [];
+
+    trList.each(function () {
+      var temp = [];
+      var tdList = $(this).find('td');
+
+      tdList.each(function (i) {
+        temp.push($(this).text());
+      });
+
+      result.push(temp);
+    });
+
+    console.log(result);
+
+    // cancel default behavir
+    return false;
+  });
+
+  comparePanel.append(screenshotButton);
+});
--- content_scripts/src/index.js
+++ content_scripts/src/index.js
@@ -1,2 +1,32 @@
 $(document).ready(() => {
+  let comparePanel = $('.button-holder:last')
+
+  const screenshotButton = $(`<button class="btn control-button btn-primary">Testray Screenshot</button>`)
+
+  screenshotButton.click(() => {
+    const resultTable = $('.table-data')
+    const trList = resultTable.find('tr')
+
+    console.log(trList)
+
+    let result = []
+
+    trList.each(function () {
+      let temp = []
+      const tdList = $(this).find('td')
+
+      tdList.each(function (i) {
+        temp.push($(this).text())
+      })
+
+      result.push(temp)
+    })
+
+    console.log(result)
+
+    // cancel default behavir
+    return false
+  })
+
+  comparePanel.append(screenshotButton)
 })
